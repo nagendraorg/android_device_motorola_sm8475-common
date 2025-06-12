328a7c0c2894e04ffc7f1e03dbf01fa21dbbcec2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-12 16:01:43 +0200

    sm8475-common: Convert overlays to RRO
    Change-Id: Id0226559b81e115a6340591e7a107d25d7ad6848

--- sm8475.mk
+++ sm8475.mk
@@ -23,13 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
+    LineageSdkCommon \
     SettingsCommon \
     SystemUIResCommon \
     TelephonyResCommon \
