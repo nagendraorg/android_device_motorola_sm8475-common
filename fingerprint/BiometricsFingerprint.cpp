4f322fb1471297bca9166ca43b8624d6b102144f
Giovanni Ricca <giovanniricca@proton.me>
2025-10-22 15:09:21 +0200

    sm8475-common: biometrics: Set `LOG_TAG` via cflag
    Change-Id: I905fd52b719e41802e5eadcdefb2839d443c9d30

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.moto_sm8475_fod"
-
 #include "BiometricsFingerprint.h"
 
 #include <android-base/file.h>
