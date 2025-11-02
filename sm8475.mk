5f4f86acf8d42d3e54d13ed6a5b46809bcbf3667
chandu078 <chandudyavanapelli03@gmail.com>
2025-11-02 15:59:54 +0000

    sm8475-common: Move manifests to vintf directory
    Change-Id: I2a82e3ae75a1293318efeea0519cfbe64eb4b364

--- sm8475.mk
+++ sm8475.mk
@@ -380,12 +380,12 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VINTF
-DEVICE_FRAMEWORK_MANIFEST_FILE += device/motorola/sm8475-common/framework_manifest.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += device/motorola/sm8475-common/vintf/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += device/motorola/sm8475-common/manifest.xml
+DEVICE_MANIFEST_FILE += device/motorola/sm8475-common/vintf/manifest.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
