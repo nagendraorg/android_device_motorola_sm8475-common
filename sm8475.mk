7ea3395ca539dfd9dbed4734b2c4318cf99daf5c
Michael Bestas <mkbestas@lineageos.org>
2026-04-14 00:08:31 +0300

    sm8475-common: gps: Move some config files to vendor
    We don't make any changes there
    
    Change-Id: I1dfaade4a86b37576e6c848732cec4af9ad97c47

--- sm8475.mk
+++ sm8475.mk
@@ -137,9 +137,7 @@ PRODUCT_PACKAGES += \
     batching.conf \
     gnss_antenna_info.conf \
     gps.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf
+    izat.conf
 
 # Health
 PRODUCT_PACKAGES += \
